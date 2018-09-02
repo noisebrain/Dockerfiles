ae0b287b55f671e7cad36f31a1a1440233332405
bozoface <noisebrain@gmail.com>
2018-09-01 20:44:51 -0700

    forgot to add prebuild.jl and startup.jl

---
+++ JuliaGPUdev/prebuild.jl
@@ -0,0 +1,20 @@
+println("running prebuild.jl")
+# todo need to add FileIO, HDF5
+
+using Pkg
+
+_pkgs = ["GZip", "ArgParse","Images","ImageMagick","IJulia"]
+for p in _pkgs
+  Pkg.add(p)
+  Pkg.build(p)
+end
+
+using GZip, ArgParse,Images,IJulia,ImageMagick
+
+Pkg.add(PackageSpec(url="https://github.com/denizyuret/AutoGrad.jl",rev="c3a91a8"))	# is 1.01
+#Pkg.add(PackageSpec(url="https://github.com/denizyuret/Knet.jl",rev="348a2fe")) # is 1.0.0 30aug18
+Pkg.add(PackageSpec(url="https://github.com/denizyuret/Knet.jl",rev="4342175")) # is 1.0.1
+#Pkg.add(PackageSpec("Knet.jl",version="1.0.1"))	# 4342175
+using Knet
+
+exit(0)
