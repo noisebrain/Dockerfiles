82c2fdcaa9fde7ef20826e3149393e2604bf8508
zilla <noisebrain@gmail.com>
2020-04-04 12:19:26 -0700

    remove env print

--- JuliaGPUdev/startup.jl
+++ JuliaGPUdev/startup.jl
@@ -1,6 +1,7 @@
 println("................startup.jl................")
 #ENV["LD_LIBRARY_PATH"] = ENV["LD_LIBRARY_PATH"]*":/root/.julia/packages/Conda/m7vem/deps/usr/lib"
-println(ENV["LD_LIBRARY_PATH"])
+# for juno, LD_LIBRARY_PATH is not set!!
+# println(ENV["LD_LIBRARY_PATH"])
 
 #if !isdir("/root/.julia/packages")
 #  run(`cp -r /PREBUILT /root/.julia`)
